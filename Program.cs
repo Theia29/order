72fb2c18273d66a095c17f4facd059aa86b89623
Theia29 <Theiyasri.A@cognizant.com>
2025-04-10 16:34:34 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using OrderModuleV2.Model;
+using OrderModuleV2.Repositories;
+using OrderModuleV2.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+
+builder.Services.AddScoped<ICartService, CartService>();
+builder.Services.AddScoped<ICartRepository, CartRepository>(); 
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
