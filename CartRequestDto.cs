2bf4a91fcf581e87ad74beefbd070e84fd3c2728
Theia29 <Theiyasri.A@cognizant.com>
2025-04-10 16:38:09 +0530

    Add files via upload

---
+++ CartRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OrderModuleV2.Models
+{
+    public class CartRequestDto
+    {
+        [Required]
+        public int CustId { get; set; }
+        [Required]
+        public List<int> ItemIds { get; set; }
+        [Required]
+        public List<int> ResIds { get; set; }
+        [Required]
+        public List<int> Quantities { get; set; }
+        [Required]
+        public List<decimal> Prices { get; set; }
+    }
+}
